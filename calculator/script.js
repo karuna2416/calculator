7d4cf7595bcafc6c6e4f12561252471ca85d23cd
karuna sarawat <87230635+karuna2416@users.noreply.github.com>
2022-08-24 04:59:05 -0700

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,25 @@
+
+let string = "";
+//put event all buttons
+let buttons = document.querySelectorAll('.button');
+//make array with the help of btn then put for each then listen click event 
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'c'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
+
+
+
